fbe0ec30e15f4a4d82cf481ddb2ce709f827131f
gustavo-sobbrero-develcode <gustavo.sobbrero@develcode.com.br>
2022-04-29 16:06:09 -0300

    Criada api com axios e testado metodo get na tela inicio

--- src/screens/Inicio/Inicio.tsx
+++ src/screens/Inicio/Inicio.tsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text } from 'react-native'
+import { api } from '../../services/api'
+interface User {
+  login: string;
+}
 
 export const Inicio: React.FC<undefined> = () => {
-  return <Text>Inicio</Text>
+
+  const [user, setUser] = useState<User>()
+
+  //GET PUT ...
+
+  useEffect(() => {
+    api.get('/users/defunkt').then((response) => {
+      setUser(response.data);
+
+    })
+  }, [])
+
+  return <Text>{user?.login}</Text>
 }
